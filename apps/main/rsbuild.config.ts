1b93482dc8b1bbb18f322f3b14d5469d2db64b5f
Zargham Khan <zargham.khan@leanix.net>
2025-02-24 01:16:30 +0100

    fix

--- apps/main/rsbuild.config.ts
+++ apps/main/rsbuild.config.ts
@@ -2,4 +2,6 @@ import { createConfig } from '@ng-rsbuild/plugin-angular';
 
 export default createConfig({
   browser: './src/main.ts',
+  inlineStylesExtension: 'scss',
+  styles: ['./src/styles.scss'],
 });
