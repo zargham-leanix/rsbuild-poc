cfccab3595578e4386a551f28bf44325ce4a3142
Zargham Khan <zargham.khan@leanix.net>
2025-02-24 20:24:35 +0100

    create library

---
+++ libs/core-ui/src/lib/core-ui/core-ui.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-core-ui',
+  imports: [CommonModule],
+  templateUrl: './core-ui.component.html',
+  styleUrl: './core-ui.component.css',
+})
+export class CoreUiComponent {}
