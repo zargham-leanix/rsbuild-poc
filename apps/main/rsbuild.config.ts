9dd8649ec6d140c26e9cc2ba77cb2d0a48de0b7e
Zargham Khan <zargham.khan@leanix.net>
2025-02-24 19:07:41 +0100

    use includePaths of sassPlugin

--- apps/main/rsbuild.config.ts
+++ apps/main/rsbuild.config.ts
@@ -1,7 +1,19 @@
 import { createConfig } from '@ng-rsbuild/plugin-angular';
+import { pluginSass } from '@rsbuild/plugin-sass';
 
-export default createConfig({
-  browser: './src/main.ts',
-  inlineStylesExtension: 'scss',
-  styles: ['./src/styles.scss'],
-});
+export default createConfig(
+  {
+    browser: './src/main.ts',
+    inlineStylesExtension: 'scss',
+    styles: ['./src/styles.scss'],
+  },
+  {
+    plugins: [
+      pluginSass({
+        sassLoaderOptions: {
+          sassOptions: { includePaths: ['libs/shared/styles'] },
+        },
+      }),
+    ],
+  }
+);
