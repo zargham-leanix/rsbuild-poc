49241a00b0ee51ab383361b01fb009cd1c8da4b5
Zargham Khan <zargham.khan@leanix.net>
2025-03-05 21:34:00 +0100

    setup rspack

--- apps/main/rsbuild.config.ts
+++
@@ -1,7 +0,0 @@
-import { createConfig } from '@ng-rsbuild/plugin-angular';
-
-export default createConfig({
-  browser: './src/main.ts',
-  inlineStylesExtension: 'scss',
-  styles: ['./src/styles.scss'],
-});
