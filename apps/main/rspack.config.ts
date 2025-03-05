151c066005829d1d8c164970863bea1b26f6a1b8
Zargham Khan <zargham.khan@leanix.net>
2025-03-05 21:34:00 +0100

    setup rspack

---
+++ apps/main/rspack.config.ts
@@ -0,0 +1,18 @@
+const { resolve } = require('path');
+const { createConfig } = require('@ng-rspack/build');
+
+module.exports = createConfig({
+  inlineStylesExtension: 'scss',
+  index: './src/index.html',
+  browser: './src/main.ts',
+  fileReplacements: [],
+  tsconfigPath: resolve(__dirname, './tsconfig.prod.json'),
+  polyfills: ['zone.js'],
+  assets: [],
+  root: __dirname,
+  scripts: [],
+  jit: false,
+  hasServer: false,
+  skipTypeChecking: false,
+  styles: ['./src/styles.scss'],
+});
